320e5726ce19a394d792cfd637aa28e7d9a27c59
VTBTS <76725953+VTBTS@users.noreply.github.com>
2021-04-05 18:56:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function imgss(){
+     window.location="IMGS.html";
+};
+
+function save()
+{
+  link = document.getElementById("link");
+  pdf = document.getElementById("selfie_image").src ;
+  link.href = image;
+  link.click();  
+};
